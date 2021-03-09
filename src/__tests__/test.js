ee25513459f73167c6dafee3be1ff9d1f4e99eb4
Pezu-git <artem_nikolaev69@mail.ru>
2021-03-09 21:22:48 +0300

    first

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -2,9 +2,13 @@
 // eslint-disable-next-line no-unused-vars
 import ErrorRepository from '../index.js';
 
-test('findErrorText_test', () => {
+test('first findErrorText_test', () => {
   const errMap = new ErrorRepository();
   expect(errMap.translate(1)).toBe('первая ошибка');
+});
+test('second findErrorText_test', () => {
+  const errMap = new ErrorRepository();
+  expect(errMap.translate(5)).toBe('пятая ошибка');
 });
 test('unknownError test', () => {
   const errMap = new ErrorRepository();
